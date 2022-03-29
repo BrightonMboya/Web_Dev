de4c7310e80b3cfde8a66c14cd2aeb93c360c9ee
BrightonMboya <b.mboya@alustudent.com>
2022-03-29 11:56:30 +0200

    finished getting started with React

--- ReactJS/react-task-tracker/src/Components/Button.js
+++ ReactJS/react-task-tracker/src/Components/Button.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types'
-export const Button = ({color, text, onClick}) => {
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <button 
-    onClick={onClick}
-    className='btn' style={{backgroundColor: color }}>{text}</button>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
   )
 }
 
 Button.defaultProps = {
-  color: 'steelBlue'
+  color: 'steelblue',
 }
 
 Button.propTypes = {
@@ -16,4 +21,5 @@ Button.propTypes = {
   color: PropTypes.string,
   onClick: PropTypes.func,
 }
+
 export default Button
