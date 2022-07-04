c0f261fffeb355a839cc2f10654a1c3ed95043a8
BrightonMboya <b.mboya@alustudent.com>
2022-07-04 11:48:36 +0200

    learned fundamentals of expressJS

--- Vanilla JavaScript/app.js
+++ Vanilla JavaScript/app.js
@@ -1,8 +1,20 @@
-const http = require("http");
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-server.listen(3000);
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(400).sendFile(path.join(__dirname, "Views", "404.html"));
+});
+
+app.listen(3000);
